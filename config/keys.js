93106dd875d558161fb66255397a0e83b3ab700c
AndrewKyu <andrewkyu95@gmail.com>
2018-06-11 21:54:33 -0700

    Created JSON web token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  monogoURI: "mongodb://andrew:vacati0n@ds153460.mlab.com:53460/devconnector"
+  monogoURI: "mongodb://andrew:vacati0n@ds153460.mlab.com:53460/devconnector",
+  secretOrKey: "secret"
 };
