23cd3ea41fa198fb05e9261b20ed2d018b0ac301
AndrewKyu <andrewkyu95@gmail.com>
2018-06-13 19:48:16 -0700

    Validation handling

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 //Loading User Model
 const User = require("../../models/User");
 
@@ -19,6 +22,13 @@ router.get("/test", (req, res) => res.json({ msg: "users works" }));
 //@access       Public
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
